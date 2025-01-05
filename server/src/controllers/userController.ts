3fc55d843bacb95390a590530ca5e54589d90f18
Jafor Iqbal <jaforiqbal5593@gmail.com>
2025-01-05 15:03:53 +0600

    userController added

---
+++ server/src/controllers/userController.ts
@@ -0,0 +1,28 @@
+import User from "../models/Users";
+import { Request, Response } from "express";
+
+export const signUpWithLogin = async (req: Request, res: Response) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to create user" });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    console.log("Email: ", email);
+    const user = await User.findOne({ where: { email } });
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch user" });
+  }
+};
