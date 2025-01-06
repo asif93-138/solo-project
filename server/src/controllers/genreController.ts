5d541021c28247b1c59f7cc42da5c9b8db0b773d
Alina <alina.hasan.aman@gmail.com>
2025-01-06 13:08:59 +0600

    remove mgs

--- server/src/controllers/genreController.ts
+++ server/src/controllers/genreController.ts
@@ -1,17 +1,6 @@
 import Genre from "../models/Genre";
 import { Request, Response } from "express";
 
-// // Route: Insert a single genre
-// app.post("/genres", async (req: Request, res: Response) => {
-// try {
-//     const genre = await Genre.create(req.body);
-//     res.status(201).json(genre);
-// } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: "Failed to create genre" });
-// }
-// });
-
 export const addGenre = async (req: Request, res: Response) => {
     try {
         const genre = await Genre.create(req.body);
@@ -21,17 +10,6 @@ export const addGenre = async (req: Request, res: Response) => {
         res.status(500).json({ error: "Failed to create genre" });
     }
 }
-
-// // Route: Read all genres
-// app.get("/genres", async (req: Request, res: Response) => {
-//   try {
-//     const genres = await Genre.findAll();
-//     res.status(200).json(genres);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: "Failed to fetch genres" });
-//   }
-// });
 
 export const getAllGenres = async (req: Request, res: Response) => {
     try {
