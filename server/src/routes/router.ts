774f3569a3e24cad9dc6ba077408b429e6cab1ab
Alina <alina.hasan.aman@gmail.com>
2025-01-06 13:19:37 +0600

    remove controller.ts route.ts

--- server/src/routes/router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getAllUsers } from "../controllers/controllers";
-const router = Router();
-
-router.get('/users', getAllUsers)
-
-
-export default router;
