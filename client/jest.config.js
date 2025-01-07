f63ed6bc054a6d9c6cb3443250da31bd35de0ca6
Jafor Iqbal <jaforiqbal5593@gmail.com>
2025-01-07 18:16:57 +0600

    jest config added

---
+++ client/jest.config.js
@@ -0,0 +1,8 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1", // Map @ to src folder
+  },
+};
