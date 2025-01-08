6eef2cc0a80d05a903ed60f67b3628f15224e457
Jafor Iqbal <jaforiqbal5593@gmail.com>
2025-01-08 16:09:57 +0600

    write create movie test

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,7 +1,7 @@
 export default {
   preset: "ts-jest",
   testEnvironment: "node",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  //setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1", // Map @ to src folder
   },
--- client/src/services/movieService.ts
+++ client/src/services/movieService.ts
@@ -1,18 +1,30 @@
-export async function createMovie(x: any) {
-    try {
-        const movieResponse = await fetch("http://localhost:3000/api/movie/", {
-            method: "POST",
-            headers: {
-              "content-type": "application/json",
-            },
-            body: JSON.stringify(x),
-          });
-          
-          const movieData = await movieResponse.json();
-          
-          return movieData.movie;
-    } catch(error) {
-        console.log(error);
-    }
+// export async function createMovie(x: any) {
+//   try {
+//     const movieResponse = await fetch("http://localhost:3000/api/movie/", {
+//       method: "POST",
+//       headers: {
+//         "content-type": "application/json",
+//       },
+//       body: JSON.stringify(x),
+//     });
 
+//     const movieData = await movieResponse.json();
+//     console.log("Services: ", movieData);
+//     return movieData.movie;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+import axios from "axios";
+
+export async function createMovie(x: any) {
+  try {
+    console.log("X: ", x);
+    const response = await axios.post("http://localhost:3000/api/movie/", x);
+    console.log("Services: ", response);
+    return response.data; // Adjust based on actual API response
+  } catch (error) {
+    throw new Error("Failed creating movie");
+  }
 }
