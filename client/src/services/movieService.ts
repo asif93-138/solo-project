2bb0b58032f0f1e21fb5d914a8c4c3968b4ca148
Alina <alina.hasan.aman@gmail.com>
2025-01-08 17:11:24 +0600

    update fetch my list

--- client/src/services/movieService.ts
+++ client/src/services/movieService.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export async function getAllMovies() {
-    const movies = await fetch('http://localhost:3000/api/movie')
-      .then((res) => res.json())
-      .then((data) => data);
-    return movies;
+  const movies = await fetch('http://localhost:3000/api/movie')
+    .then((res) => res.json())
+    .then((data) => data);
+  return movies;
 }
 
 export async function getAllGenres() {
@@ -27,13 +27,13 @@ export async function searchMovies(searchType: any, value: any) {
   return results;
 }
 
-export function getMyList(user_id:any, setData:any) {
-  fetch("http://localhost:3000/moviesFromUser/" + user_id)
+export function getMyList(user_id: any, setData: any) {
+  fetch("http://localhost:3000/api/movie/user/" + user_id)
     .then((res) => res.json())
     .then((data) => setData(data));
 }
 
-export function getMovieDetails(movie_id:any, setDataObj:any) {
+export function getMovieDetails(movie_id: any, setDataObj: any) {
   fetch("http://localhost:3000/api/movie/" + movie_id)
     .then((res) => res.json())
     .then((data) => setDataObj(data));
@@ -41,68 +41,68 @@ export function getMovieDetails(movie_id:any, setDataObj:any) {
 
 export async function createRatingAndReview(data: any) {
   const response = await fetch("http://localhost:3000/api/review/", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    const result = await response.json();
-    return result;
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  const result = await response.json();
+  return result;
 }
 
 export async function updateRatingAndReview(id: any, data: any) {
   const response = await fetch("http://localhost:3000/api/review/" + id, {
-      method: "PUT",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    const result = await response.json();
-    return result;
+    method: "PUT",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  const result = await response.json();
+  return result;
 }
 
 export async function createMovie(data: any) {
-        const movieResponse = await fetch("http://localhost:3000/api/movie/", {
-            method: "POST",
-            headers: {
-              "content-type": "application/json",
-            },
-            body: JSON.stringify(data),
-          });
-          const movieData = await movieResponse.json();
-          return movieData.movie;
+  const movieResponse = await fetch("http://localhost:3000/api/movie/", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  const movieData = await movieResponse.json();
+  return movieData.movie;
 }
 
 export async function createNewGenre(data: any) {
   const response = await fetch("http://localhost:3000/api/genre/", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    const result = await response.json();
-    return result;
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  const result = await response.json();
+  return result;
 }
 
 export async function deleteMovie(id: any) {
   const response = await fetch("http://localhost:3000/api/movie/" + id, {
-      method: "DELETE"
-    });
-    const result = await response.json();
-    return result;
+    method: "DELETE"
+  });
+  const result = await response.json();
+  return result;
 }
 
 export async function updateMovie(id: any, data: any) {
   const response = await fetch("http://localhost:3000/api/movie/" + id, {
-      method: "PUT",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    const result = await response.json();
-    return result;
+    method: "PUT",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  const result = await response.json();
+  return result;
 }
