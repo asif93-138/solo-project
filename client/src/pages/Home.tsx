9e26ca528e12d6570bfba145e78448c2542c2ca5
Jafor Iqbal <jaforiqbal5593@gmail.com>
2025-01-15 12:20:49 +0600

    change in home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,21 +1,22 @@
-import '../index.css';
-import { useEffect, useState, useContext, FormEvent } from 'react';
+import "../index.css";
+import { Movie, Genre } from "../interfaces/home";
+import MovieCards from "../components/MovieCards";
 import { UserContext } from "../contexts/UserContext";
-import { Movie, Genre } from '../interfaces/home';
-import MovieCards from '../components/MovieCards';
-import { getAllGenres } from '../services/genreService';
-import { getAllMovies, searchMovies } from '../services/movieService';
+import { getAllGenres } from "../services/genreService";
+import { useEffect, useState, useContext, FormEvent } from "react";
+import { getAllMovies, searchMovies } from "../services/movieService";
 
 function App() {
-  const [data, setData] = useState<Movie[]>([]);
-  const [initialResults, setInitialResults] = useState<Movie[]>([]);
-  const [searchTitle, setSearchTitle] = useState('');
-  const [searchGenre, setSearchGenre] = useState('');
-  const [genres, setGenres] = useState<Genre[]>([]);
-  const [showNRF, setShowNRF] = useState(false);
-  const [showSH, setShowSH] = useState(false);
-  const [showSCB, setShowSCB] = useState(false);
   const context = useContext(UserContext);
+  const [showSH, setShowSH] = useState(false);
+  const [data, setData] = useState<Movie[]>([]);
+  const [showSCB, setShowSCB] = useState(false);
+  const [showNRF, setShowNRF] = useState(false);
+  const [searchTitle, setSearchTitle] = useState("");
+  const [searchGenre, setSearchGenre] = useState("");
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [initialResults, setInitialResults] = useState<Movie[]>([]);
+
   async function fetchingMovies() {
     const results = await getAllMovies();
     setInitialResults(results);
@@ -33,124 +34,136 @@ function App() {
   const handleSearch = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     // console.log(searchTitle.trim(), searchGenre);
-          try {
-          const searchData: Movie[] = await searchMovies(searchTitle.trim(), searchGenre);
-          if (Array.isArray(searchData)) {
-            setData(searchData);
-            setShowNRF(false);
-          } else {
-            setData([]);
-            setShowNRF(true);
-          }
-          setShowSH(true);
-          setShowSCB(true);
-        } catch (err) {
-          console.error("Error fetching data:", err);
-          setData([]);
-        }
-
-      // if (searchType == 'title') {
-      //   try {
-      //     const searchData: Movie[] = await searchMovies(searchType, searchValue.trim());
-      //     if (Array.isArray(searchData)) {
-      //       setData(searchData);
-      //       setShowNRF(false);
-      //     } else {
-      //       setData([]);
-      //       setShowNRF(true);
-      //     }
-      //     setShowSH(true);
-      //     setShowSCB(true);
-      //   } catch (err) {
-      //     console.error("Error fetching data:", err);
-      //     setData([]);
-      //   }
-      // } else if (searchType == 'genre') {
-      //   try {
-      //     const searchData: Movie[] = await searchMovies(searchType, searchValue.trim());
-      //     if (Array.isArray(searchData)) {
-      //       setData(searchData);
-      //       setShowNRF(false);
-      //     } else {
-      //       setData([]);
-      //       setShowNRF(true);
-      //     }
-      //     setShowSH(true);
-      //     setShowSCB(true);
-      //   } catch (err) {
-      //     console.error("Error fetching data:", err);
-      //     setData([]);
-      //   }
-      // }
+    try {
+      const searchData: Movie[] = await searchMovies(
+        searchTitle.trim(),
+        searchGenre
+      );
+      if (Array.isArray(searchData)) {
+        setData(searchData);
+        setShowNRF(false);
+      } else {
+        setData([]);
+        setShowNRF(true);
+      }
+      setShowSH(true);
+      setShowSCB(true);
+    } catch (err) {
+      console.error("Error fetching data:", err);
+      setData([]);
+    }
   };
 
   return (
     <section className="bg-black pt-5 pb-10 min-h-screen">
       {/* Open the modal using document.getElementById('ID').showModal() method */}
-<button className="btn hidden" onClick={()=>document.getElementById('my_modal_x')?.classList.add('modal-open')}>open modal</button>
-<dialog id="my_modal_x" className="modal">
-  <div className="modal-box">
-    <h3 className="font-bold text-lg">Hello!</h3>
-    <p className="py-4">Press ESC key or click the button below to close</p>
-    <div className="modal-action">
-      <form method="dialog">
-        {/* if there is a button in form, it will close the modal */}
-        <button className="btn" onClick={()=>document.getElementById('my_modal_x')?.classList.remove('modal-open')}>Close</button>
-      </form>
-    </div>
-  </div>
-</dialog>
+      <button
+        className="btn hidden"
+        onClick={() =>
+          document.getElementById("my_modal_x")?.classList.add("modal-open")
+        }
+      >
+        open modal
+      </button>
+      <dialog id="my_modal_x" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">
+            Press ESC key or click the button below to close
+          </p>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button
+                className="btn"
+                onClick={() =>
+                  document
+                    .getElementById("my_modal_x")
+                    ?.classList.remove("modal-open")
+                }
+              >
+                Close
+              </button>
+            </form>
+          </div>
+        </div>
+      </dialog>
       <div className="items-end mb-10 flex w-3/4 mx-auto justify-center">
-      <form onSubmit={handleSearch} className="flex gap-4 rounded-lg">
-      <div className="">
-        <label htmlFor="title" className="label">
-          <span className="label-text text-white">Movie Title</span>
-        </label>
-        <input
-          type="text"
-          id="title"
-          placeholder="Search by title..."
-          className="input input-bordered w-full"
-          value={searchTitle}
-          onChange={e => setSearchTitle(e.target.value)}
-        />
-      </div>
-      <div className="">
-        <label htmlFor="genre" className="label">
-          <span className="label-text text-white">Genre</span>
-        </label>
-        <select
-          id="genre"
-          className="select select-bordered w-full"
-          value={searchGenre}
-          onChange={e => setSearchGenre(e.target.value)}
+        <form onSubmit={handleSearch} className="flex gap-4 rounded-lg">
+          <div className="">
+            <label htmlFor="title" className="label">
+              <span className="label-text text-white">Movie Title</span>
+            </label>
+            <input
+              type="text"
+              id="title"
+              placeholder="Search by title..."
+              className="input input-bordered w-full"
+              value={searchTitle}
+              onChange={(e) => setSearchTitle(e.target.value)}
+            />
+          </div>
+          <div className="">
+            <label htmlFor="genre" className="label">
+              <span className="label-text text-white">Genre</span>
+            </label>
+            <select
+              id="genre"
+              className="select select-bordered w-full"
+              value={searchGenre}
+              onChange={(e) => setSearchGenre(e.target.value)}
+            >
+              <option value="" disabled>
+                All Genres
+              </option>
+              {genres.map((g) => (
+                <option key={g.genre_id} value={g.genre}>
+                  {g.genre}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="form-control sm:self-end">
+            <button type="submit" className="btn">
+              Search
+            </button>
+          </div>
+        </form>
+        <button
+          id="scb"
+          type="button"
+          className={showSCB ? "btn ms-10" : "btn ms-10 hidden"}
+          onClick={() => {
+            setSearchTitle("");
+            setSearchGenre("");
+            setData(initialResults);
+            setShowSH(false);
+            setShowNRF(false);
+            setShowSCB(false);
+          }}
         >
-          <option value="" disabled>All Genres</option>
-          {genres.map((g) => (
-            <option key={g.genre_id} value={g.genre}>
-              {g.genre}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="form-control sm:self-end">
-        <button type="submit" className="btn">
-          Search
+          Return
         </button>
       </div>
-    </form>
-        <button id='scb' type='button' className={showSCB ? 'btn ms-10' : 'btn ms-10 hidden'} onClick={() => {
-          setSearchTitle('');
-          setSearchGenre('');
-          setData(initialResults);
-          setShowSH(false);
-          setShowNRF(false);
-          setShowSCB(false);
-        }}>Return</button>
-      </div>
 
-      <h4 id='sh' className={showSH ? 'text-white text-2xl text-center mb-6' : 'text-white text-2xl text-center mb-6 hidden'}>Search Results!</h4>
-      <p id='nrf' className={showNRF ? 'text-white text-center' : 'text-white text-center hidden'}>No results found..</p>
+      <h4
+        id="sh"
+        className={
+          showSH
+            ? "text-white text-2xl text-center mb-6"
+            : "text-white text-2xl text-center mb-6 hidden"
+        }
+      >
+        Search Results!
+      </h4>
+      <p
+        id="nrf"
+        className={
+          showNRF ? "text-white text-center" : "text-white text-center hidden"
+        }
+      >
+        No results found..
+      </p>
       {data.length > 0 && <MovieCards dataObj={data} />}
     </section>
   );
