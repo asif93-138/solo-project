06efd2b027179d257a6965505036e2d121e979c5
Alina <alina.hasan.aman@gmail.com>
2025-01-15 17:25:24 +0600

    add details

--- client/angular/src/app/details/details.component.ts
+++ client/angular/src/app/details/details.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
-export class DetailsComponent {
 
+export class DetailsComponent {
+  // route: ActivatedRoute = inject(ActivatedRoute);
+  // movieService = inject(movieService);
+  // movieLocation: MovieLocation | undefined;
+  applyForm = new FormGroup({
+    //some code
+  });
+  constructor() {
+    // const movieLocationId = Number(this.route.snapshot.params['id']);
+    // this.movieService.getMovieLocationById(movieLocationId).then(movieLocation => {
+    //   this.movieLocation = movieLocation;
+    // });
+  }
+  submitApplication() {
+    // this.movieService.submitApplication(
+    //some Code
+    // );
+  }
 }
--- client/angular/src/app/routes.ts
+++ client/angular/src/app/routes.ts
@@ -2,6 +2,7 @@ import { Routes } from "@angular/router"
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
+import { DetailsComponent } from "./details/details.component";
 
 const routeConfig: Routes = [
     {
@@ -18,6 +19,11 @@ const routeConfig: Routes = [
         path: 'register',
         component: RegisterComponent,
         title: 'Register Page'
+    },
+    {
+        path: 'details/:id',
+        component: DetailsComponent,
+        title: 'Details Page'
     }
 ];
 
