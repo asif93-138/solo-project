92aa5cfd8a494bdea4d770f68665439e67eedbbe
asif93-138 <asif93@student.sust.edu>
2025-01-15 17:18:30 +0600

    registration page going on!

--- client/angular/src/app/register/register.component.ts
+++ client/angular/src/app/register/register.component.ts
@@ -1,26 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { UserService } from '../services/userServices/user.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   template: `
     <div class="p-8">
-      <form class="border-2 p-6 rounded-lg w-2/5 mx-auto mt-4">
+      <form class="border-2 p-6 rounded-lg w-2/5 mx-auto mt-4" [formGroup]="applyForm" (submit)="submitApplication()">
         <p class="text-2xl mb-4 font-medium">Register</p>
         <label for="name" class="font-medium">Name</label><br />
-        <input type="text" id="name" name="name" class="border border-gray-400 rounded text-sm p-1 w-full" required /><br />
+        <input type="text" id="name" formControlName="name" class="border border-gray-400 rounded text-sm p-1 w-full" required /><br />
         <label for="email" class="font-medium">Email</label><br />
-        <input type="email" id="email" name="email" class="border border-gray-400 rounded text-sm p-1 w-full" required /><br />
+        <input type="email" id="email" formControlName="email" class="border border-gray-400 rounded text-sm p-1 w-full" required /><br />
         <!-- {showUniqueEmailWarn && <p class="text-red-600">This email is already used!</p>} -->
         <label for="pass" class="font-medium">Password</label><br />
-        <input type="password" id="pass" name="pass" class="border border-gray-400 rounded text-sm p-1 w-full" required /><br />
+        <input type="password" id="pass" formControlName="pass" class="border border-gray-400 rounded text-sm p-1 w-full" required /><br />
         <!-- <p id="passWarn" class={showPassWarn ? "text-red-600" : "text-red-600 hidden"}>Password must be at least 8 characters long!</p> -->
         <input type="submit" class="btn btn-warning my-4 w-full" value="Register" />
         <p class="text-center mb-2 text-slate-400">Already have an account?</p>
         <hr />
-        <!-- <Link to='/login'><button type="button" class="btn mt-4 w-full">Login</button></Link> -->
+        <a [routerLink]="['/login']"><button type="button" class="btn mt-4 w-full">Login</button></a>
       </form>
       <div class="toast toast-top toast-end">
         <!-- <div class={showSecondToast ? "alert alert-success block" : "alert alert-success hidden"}>
@@ -35,5 +39,21 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  authenticationService = inject(UserService);
+  applyForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    pass: new FormControl(''),
+  });
+  async submitApplication() {
+    console.log(this.applyForm.value.name, this.applyForm.value.email, this.applyForm.value.pass);
+    // const res = await this.authenticationService.userLogin(this.applyForm.value.email, this.applyForm.value.pass);
+    // if (res.user_id && res.password == this.applyForm.value.pass) {
+    //   alert('login successful!');
+    //   // Reset the form fields after submission
+    //   this.applyForm.reset();
+    // }
+  }
 
 }
